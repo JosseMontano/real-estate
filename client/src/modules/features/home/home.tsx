83927de417dff1b912c3612b70f21eee7c1e589d
Jose Zambrana <eljosema505@gmail.com>
2024-10-24 19:25:04 -0400

    feat: Make questions

--- client/src/modules/features/home/home.tsx
+++ client/src/modules/features/home/home.tsx
@@ -1,38 +1,9 @@
-import { useForm } from "@/core/hooks/useForm";
-import { questionSchema } from "./validations/question.schema";
-import { addQuestionToDB } from "./api/endpoints";
-import { Input } from "@/core/components/form/input";
-import FormComponent from "@/core/components/form/form";
+import { Questions } from "./components/question";
 
 export const HomePage = () => {
-  const {
-    register,
-    handleOnSubmit,
-    errors,
-    isPending: isPendingQuestion,
-  } = useForm({
-    schema: questionSchema,
-    form: async (data) => {
-      await addQuestionToDB(data);
-    },
-  });
-
   return (
     <div>
-      <FormComponent
-        isPending={isPendingQuestion}
-        handleOnSubmit={handleOnSubmit}
-        btnText="Guardar"
-        children={
-          <>
-            <Input
-              text="Pregunta"
-              error={errors.question}
-              register={register("question")}
-            />
-          </>
-        }
-      />
+      <Questions />
     </div>
   );
 };
