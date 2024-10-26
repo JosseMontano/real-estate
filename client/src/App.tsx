2fe326d2f87ec53ed116f4e579450ec621f04e06
Jose Zambrana <eljosema505@gmail.com>
2024-10-25 20:55:31 -0400

    feat: Change nams of folders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { AuthPage } from "./modules/features/auth/auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Toaster } from 'sonner'
-import DashboardPage from "@/features/dashboard/dashboard";
+import DashboardPage from "@/features/profile/profile";
 
 export const queryClient = new QueryClient();
 export type Routes = "/" | "/dashboard" | "/auth";
@@ -16,7 +16,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/profile" element={<DashboardPage />} />
           <Route path="/auth" element={<AuthPage />} />
         </Routes>
       </Router>
