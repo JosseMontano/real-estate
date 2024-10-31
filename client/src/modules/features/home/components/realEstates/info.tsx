077f65577df5adb7de447df64324533e4bc29b3a
Jose Zambrana <eljosema505@gmail.com>
2024-10-31 17:53:55 -0400

    refactor: Replace by global button the homePage

--- client/src/modules/features/home/components/realEstates/info.tsx
+++ client/src/modules/features/home/components/realEstates/info.tsx
@@ -1,15 +1,22 @@
 import { RealEstate } from "@/shared/types/realEstate";
 import { State } from "../sectionRealEstates";
 import { Language } from "@/core/store/language";
+import Btn from "@/core/components/form/button";
 
 type ParamsType = {
   index: number;
   item: RealEstate;
   states: State[];
   language: Language;
-  handleStateModal: (v:RealEstate) => void
+  handleStateModal: (v: RealEstate) => void;
 };
-export const Info = ({ index, item, language, states, handleStateModal }: ParamsType) => {
+export const Info = ({
+  index,
+  item,
+  language,
+  states,
+  handleStateModal,
+}: ParamsType) => {
   return (
     <>
       {states[index] === "info" && (
@@ -20,10 +27,11 @@ export const Info = ({ index, item, language, states, handleStateModal }: Params
           <p className="mt-4 text-base md:text-lg lg:text-xl text-gray-600 max-w-xs md:max-w-md lg:max-w-lg text-justify">
             {item.title[language]}
           </p>
-          <button className="mt-6 bg-purple-600 text-white py-2 px-6 rounded-full shadow-md hover:bg-purple-700">
-            Ver más
+
+          <Btn text="Ver mas" className="w-[150px]" isPending={false} />
+          <button onClick={() => handleStateModal(item)}>
+            Agregar comentario
           </button>
-          <button onClick={()=>handleStateModal(item)}>Agregar comentario</button>
         </>
       )}
     </>
