aa5c08a3df8a4f263ff1344ad71123482c9bc75b
Jose Zambrana <eljosema505@gmail.com>
2024-12-03 17:25:19 -0400

    feat: Navigation in rn

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,34 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/AntDesign';
 import { AuthPage } from "./modules/features/auth/auth";
+import {HomePage} from "./modules/features/home/home"
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <AuthPage />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+
+            return <Icon name={"rightcircleo"} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Profile" component={AuthPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
